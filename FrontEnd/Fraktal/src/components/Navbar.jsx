8a32387b6bc5360dc8a545a4f8416c5aa27f3c01
joacobarsky <114594873+joacobarsky@users.noreply.github.com>
2024-05-14 12:10:28 -0300

    hola

--- FrontEnd/Fraktal/src/components/Navbar.jsx
+++ FrontEnd/Fraktal/src/components/Navbar.jsx
@@ -1,12 +1,35 @@
 import '../style.css';
+import '../font.css'
+import logoK from './K.png';
+import logo from './sexoso.png';
+
+
 export const Navbar = () => {
     return (
         <header>
-            <div class="button header">Tokens</div>
-            <div class="button header">Sturtups</div>
-            <h1>Fraktal</h1>
-            <div class="button header">Market</div>
-            <div class="button header">Wallet</div>
+             <div className="caja">
+                <img src={logo} alt="Logo" className="logito" />
+            </div>
+            <div className="stuff">Market</div>
+            <div className="stuff">Token</div>
+            <div className="stuff">Trade</div>
+            <div className="stuff">Wallet</div>
+            <div className="stuff">Global</div>
+            <div className="stuff">News</div>
+
+            <div className="celu">
+            <div className="cajaK">
+                <img src={logoK} alt="Logo" className="logitoK" />
+            </div>
+            <label class="container">
+            <input type="checkbox"/>
+            <div class="checkmark">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            </label>
+            </div>
         </header>
     );
 }
---
+++ FrontEnd/Fraktal/src/components/Pag1.jsx
@@ -0,0 +1,10 @@
+import '../style1.css';
+import '../font.css'
+
+
+export const Pag1 = () => {
+    return (
+        <section>
+        </section>
+    );
+}
