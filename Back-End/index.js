1f51658ee4b3b1b5da879ffd90b126d8ad1ab452
Yaguarete4 <111437151+Yaguarete4@users.noreply.github.com>
2024-06-11 10:25:31 -0300

    Subir vercel

--- Back-End/index.js
+++ Back-End/index.js
@@ -5,7 +5,9 @@ const app = express();
 app.use(express.json());
 
 app.use('/company', require('./Routers/company'));
-app.get('/', (_, _) => console.log("API is running..."));
+
+
+app.get('/', (req, res) => console.log("API is running..."));
 
 app.listen(3000, () => {
     console.log("Server listening on Port 3000")
