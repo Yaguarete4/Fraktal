3bc20a1c81abcda38ee8aa276267e0340aea1730
Yaguarete4 <111437151+Yaguarete4@users.noreply.github.com>
2024-06-11 10:22:43 -0300

    Deployment

--- Back-End/index.js
+++ Back-End/index.js
@@ -4,8 +4,8 @@ const app = express();
 
 app.use(express.json());
 
-app.use('/', (_, _) => console.log("API is running..."));
 app.use('/company', require('./Routers/company'));
+app.get('/', (_, _) => console.log("API is running..."));
 
 app.listen(3000, () => {
     console.log("Server listening on Port 3000")
