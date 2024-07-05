aec7c0d5b7dc60645859042f02bd13a089dcda05
CHAMI LUCA NAHUEN <47338591@ort>
2024-07-05 11:04:16 -0300

    Crear tokens y empresas en una URL

--- Back-End/Routers/company.js
+++ Back-End/Routers/company.js
@@ -40,13 +40,20 @@ router.post("/add", async (req, res) => {
         res.status(400).send("Token ID field must not be empty");
         return;
     }
+    if(!req.body.publicKey) return res.status(400).send("publicKey field must not be empty");
+    if(!req.body.tokenAmount) return res.status(400).send("tokenAmount field must not be empty or equal to 0");
+
+    //chaeck if tokenID already exist
+    const validTokenID = await makeQuery('SELECT "tokenID" FROM company WHERE "tokenID" = $1', [req.body.tokenID]);
+    if(validTokenID.rows.length !== 0) return res.status(400).send("tokenID already exist");
+
+    const makeToken = await createToken(req.body.publicKey, req.body.tokenID, req.body.tokenAmount, '0x');
+    if(!makeToken) return res.status(500).send("Something whent wrong when creating token");
 
     const result = await makeQuery('INSERT INTO company (name, description, members, sector, "imageURL", "tokenBenefits", "tokenImageURL", "tokenID") VALUES ($1, $2, $3, $4, $5, $6, $7, $8)', [req.body.name, req.body.description, req.body.members, req.body.sector, req.body.imageURL, req.body.tokenBenefits, req.body.tokenImageURL, req.body.tokenID]);
 
-    if(!result) {
-        res.sendStatus(400);
-        return;
-    }
+    if(!result) return res.status(400).send("Error while registering data in the database");
+
     res.sendStatus(200);
 })
 
@@ -59,6 +66,24 @@ router.get('/all', async (req, res) => {
     res.json(result.rows).status(200);
 
     createToken();
+});
+
+router.post('/balance', async (req, res) => {
+    const publicKey = req.body.publicKey;
+    const tokenID = req.body.tokenID;
+
+    if(!publicKey || tokenID == null) {
+        res.status(400).send("publicKey or tokenID can't be empty");
+        return;
+    }
+
+    const validTokenID = await makeQuery('SELECT "tokenID" FROM company WHERE "tokenID" = $1', [tokenID]);
+    if(!validTokenID) return res.status(400).send("tokenID doesn't exist or is invalid");
+
+    const balance = await getBalance(publicKey, tokenID);
+    if(balance == null) return res.status(500).send("Something went wrong when getting balance");
+
+    return await res.status(200).send(`${balance}`);
 });
 
 const makeQuery = async (query, params) => {
@@ -77,26 +102,46 @@ const makeQuery = async (query, params) => {
     return result;
 }
 
-const createToken = async () => {
+// If the function returns false is because something has failed in the creation of the token
+const createToken = async (publicKey, tokenID, amount, data) => {
     const provider = new ethers.InfuraProvider('sepolia', process.env.INFURA_API);
-    const signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
+    const signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
     const abi = await getAbi('./ContractABI/MyToken.json');
     const contract = new ethers.Contract(CONTRACT_ADDRESS, abi, provider);
     const contractSigner = contract.connect(signer);
 
     try {
-        const tx = await contractSigner.mint('0x6Fdc66cf1c2D108e3eAe95DfBa6FeffCcF90F932', 0, 50, '0x');
+        const tx = await contractSigner.mint(publicKey, tokenID, amount, data);
         const receipt = await tx.wait();
 
-        if(receipt.status === 1) console.log('Mint succesful');
-        else console.log('Mint failed');
-
-        const numero = await contract.balanceOf('0x6Fdc66cf1c2D108e3eAe95DfBa6FeffCcF90F932', 0);
-        console.log(numero)
+        if(receipt.status === 1) {
+            console.log('Mint succesful');
+            return true;
+        }
+        else { 
+            console.log('Mint failed');
+            return false;
+        }
 
     } catch (error) {
         console.error(`Error minting token: ${error}`);
+        return false;
     }
+}
+
+const getBalance = async (publicKey, tokenID) => {
+    const provider = new ethers.InfuraProvider('sepolia', process.env.INFURA_API);
+    const abi = await getAbi('./ContractABI/MyToken.json');
+    const contract = new ethers.Contract(CONTRACT_ADDRESS, abi, provider);
+    let balance;
+    try {
+        balance = await contract.balanceOf(publicKey, tokenID);
+
+    } catch(error) {
+        console.error(`Error getting balance: ${error}`);
+    }
+
+    return balance;
 }
 
 const getAbi = async (path) => {
