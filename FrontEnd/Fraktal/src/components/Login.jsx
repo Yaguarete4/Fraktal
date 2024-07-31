209eddb33d9352848e24d64afac5917a3172d7f2
joacobarsky <114594873+joacobarsky@users.noreply.github.com>
2024-07-31 15:53:19 -0300

    a

--- FrontEnd/Fraktal/src/components/Login.jsx
+++ FrontEnd/Fraktal/src/components/Login.jsx
@@ -5,20 +5,16 @@ import '../css/login.css';
 export const Login = () => {
 
     return (
-    <div className="form-container">
-        <div className="form-control">
-            <input type="value" required />
-            <label>
-            <span style={{ transitionDelay: '100ms' }}>Username</span>
-            </label>
+        <div className="loginContainer">
+            <div className="titu">Registrarse</div>
+            <input placeholder="Nombre de usuario" class="input" name="text" type="text"></input>
+            <input placeholder="Mail" class="input" name="text" type="text"></input>
+            <input placeholder="Contraseña" class="input" name="text" type="text"></input>
+            <input placeholder="Confirmar constraseña" class="input" name="text" type="text"></input>
+            <button className="regi">Registrarse</button>            
+            <div className="caja-inicio-sesion">
+                <div className="inicio-sesion">¿Ya tienes una cuenta?</div>
+            </div>          
         </div>
-        <div className="form-control">
-            <input type="value" required />
-            <label>
-            <span style={{ transitionDelay: '100ms' }}>Password</span>
-            </label>
-        </div>
-    </div>
-        
   );
 };
