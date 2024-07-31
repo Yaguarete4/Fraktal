198c069e9612ea0dc42c603275f49bd3444b3fea
Matías Buñes <47572115@est.ort.edu.ar>
2024-07-31 12:07:23 -0300

    Subir metadata casi terminado

---
+++ blockchain-hardhat/test/Token.js
@@ -0,0 +1,12 @@
+const { expect } = require("chai");
+
+describe("Token contract", function () {
+  it("Only FraktalAccount and the owner can execute mint", async function () {
+    const [owner] = await ethers.getSigners();
+
+    const hardhatToken = await ethers.deployContract("Token");
+
+    const ownerBalance = await hardhatToken.balanceOf(owner.address);
+    expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+  });
+});
