d6ae29a12d00165337cf59dfcd8ed77ce034ccb2
gpbarsky <gpbarsky@yahoo.com>
2024-08-02 19:16:16 -0300

    logo google

--- FrontEnd/Fraktal/src/components/Login.jsx
+++ FrontEnd/Fraktal/src/components/Login.jsx
@@ -1,23 +1,25 @@
-
 import React, { useEffect } from 'react';
 import '../css/login.css';
 import { Link } from 'react-router-dom';
-
+import logoGoogle from '../img/google.svg';
+    
 export const Login = () => {
-
     return (
         <div className="loginContainer">
             <div className="titu">Registrarse</div>
-            <input placeholder="Nombre de usuario" class="input" name="text" type="text"></input>
-            <input placeholder="Mail" class="input" name="text" type="text"></input>
-            <input placeholder="Contraseña" class="input" name="text" type="text"></input>
-            <input placeholder="Confirmar constraseña" class="input" name="text" type="text"></input>
+            <input placeholder="Nombre de usuario" className="input" name="text" type="text"></input>
+            <input placeholder="Mail" className="input" name="text" type="text"></input>
+            <input placeholder="Contraseña" className="input" name="text" type="text"></input>
+            <input placeholder="Confirmar constraseña" className="input" name="text" type="text"></input>
             <button className="regi">Registrarse</button>            
             <div className="caja-inicio-sesion">
                 <div className="inicio-sesion">¿Ya tienes una cuenta?</div>
-                <Link to="/signup" className="linki">Iniciar sesion</Link>
+                <Link to="/signup" className="linki">Iniciar sesión</Link>
             </div>      
-            <button className="but">Registrate con Google</button>    
+            <button className="but">
+                <img src={logoGoogle} alt="Logo de Google" style={{ marginRight: '8px', verticalAlign: 'middle' }} />
+                Regístrate con Google
+            </button>    
         </div>
-  );
+    );
 };
