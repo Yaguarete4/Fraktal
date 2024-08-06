fe78ab1f19d479c722ded2b036bdf47cf8191860
Yaguarete4 <111437151+Yaguarete4@users.noreply.github.com>
2024-08-06 15:43:48 -0300

    probando fetch

--- FrontEnd/Fraktal/src/components/Login.jsx
+++ FrontEnd/Fraktal/src/components/Login.jsx
@@ -1,17 +1,44 @@
 import React, { useEffect } from 'react';
 import '../css/login.css';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import logoGoogle from '../img/google.svg';
     
 export const Login = () => {
+    const [formValues, setFormValues] = useState({
+        username: '',
+        email: '',
+        name: 'Joaquin',
+        surname: 'Barsky',
+        password: '',
+        confirmPassword: ''
+    })
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setFormValues({ ...formValues, [name]: value });
+    };
+
+    const handleSubmit = async () => {
+        const response = await fetch('https://fraktalapi.vercel.app/auth/register', {
+            headers: {
+                "Content-Type": "application/json",
+            },
+            method: "POST",
+            body: formValues
+        })
+
+        console.log(response);
+    };
+
     return (
         <div className="loginContainer">
             <div className="titu">Registrarse</div>
-            <input placeholder="Nombre de usuario" className="input" name="text" type="text"></input>
-            <input placeholder="Mail" className="input" name="text" type="text"></input>
-            <input placeholder="Contraseña" className="input" name="text" type="text"></input>
-            <input placeholder="Confirmar constraseña" className="input" name="text" type="text"></input>
-            <button className="regi">Registrarse</button>            
+            <input placeholder="Nombre de usuario" className="input" name="username" type="text" onChange={handleInputChange}></input>
+            <input placeholder="Mail" className="input" name="email" type="text" onChange={handleInputChange}></input>
+            <input placeholder="Contraseña" className="input" name="password" type="text" onChange={handleInputChange}></input>
+            <input placeholder="Confirmar constraseña" className="input" name="confirmPassword" type="text" onChange={handleInputChange}></input>
+            <button className="regi" onClick={handleSubmit}>Registrarse</button>            
             <div className="caja-inicio-sesion">
                 <div className="inicio-sesion">¿Ya tienes una cuenta?</div>
                 <Link to="/signup" className="linki">Iniciar sesión</Link>
