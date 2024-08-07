5c48678a61f06ebecc037683b11434f6609d47eb
Yaguarete4 <111437151+Yaguarete4@users.noreply.github.com>
2024-08-07 10:59:22 -0300

    probando cors en back

--- Back-End/index.js
+++ Back-End/index.js
@@ -1,6 +1,12 @@
 require('dotenv').config;
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+app.use(cors({
+    origin: 'http://fraktal.vercel.app',
+    optionsSuccessStatus: 200
+}));
 
 app.use(express.json());
 
