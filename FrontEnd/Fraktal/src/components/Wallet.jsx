e8bba0706c001b1a6a7d75ee89abddd692bb156f
joacobarsky <114594873+joacobarsky@users.noreply.github.com>
2024-08-09 14:10:13 -0300

    wallet

--- FrontEnd/Fraktal/src/components/Wallet.jsx
+++ FrontEnd/Fraktal/src/components/Wallet.jsx
@@ -24,20 +24,41 @@ export const Wallet = () => {
             <div className="plata">$ 1.683,36</div>
             <div className="profit">7,34% (+$43,22)</div>
             <div className="caja-but">
-              <div className="but-portafolio">Comprar</div>
-              <div className="but-portafolio2">Vender</div>
-            </div>
+              <div className="but-portafolio">Ingresar</div>
+              <div className="but-portafolio2">Retirar</div>
+            </div>              
+            <div className="caja-rendimiento">
+                <div className="rendimiento-titu">Rendimientos</div>
+                <div className="caja-chiki">
+                  <div className="ren">
+                    <div className="ren-nombre">Bitcoin BTC</div>
+                  </div>
+                  <div className="ren">
+                    <div className="ren-nombre">Bitcoin B</div>
+                  </div>
+                  <div className="ren">
+                    <div className="ren-nombre">Bitcoin B</div>
+                  </div>
+                </div>
+              </div>
             <div className="caja-reparticion">
               <div className="dona-container">
                 <div className="dona"></div>
                 <div className="dona-text">$ 1.683,36</div>
               </div>
+
               <div className="caja-porcentaje">
                 <div className="cuadra">
                   <div className="c1"></div>
                   <div className="c2"></div>
                   <div className="c3"></div>
                   <div className="c4"></div>
+                </div>
+                <div className="nombres-tokens">
+                  <div className="t1">CHM</div>
+                  <div className="t1">CHM</div>
+                  <div className="t1">CHM</div>
+                  <div className="t1">CHM</div>
                 </div>
               </div>
             </div>
