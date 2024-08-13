01137347ba0889403c34a96d30d9e5978571bcb4
Yaguarete4 <111437151+Yaguarete4@users.noreply.github.com>
2024-08-13 10:13:36 -0300

    implementacion de credenciales cors

--- Back-End/index.js
+++ Back-End/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors({
     origin: ['https://fraktal.vercel.app', 'http://localhost:5173'],
     credentials: true,
-    allowedHeaders: 'Set-Cookie',
+    allowedHeaders: '*',
     optionsSuccessStatus: 200
 }));
 
