253d907d0b575c821e3659497edca4330a2012d8
joacobarsky <joaquibarsky@gmail.com>
2024-08-23 21:57:31 -0300

    form listo

--- FrontEnd/Fraktal/src/pages/TokenRegisterPage.jsx
+++ FrontEnd/Fraktal/src/pages/TokenRegisterPage.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useRef } from 'react';
 import { Navbar } from "../components/Navbar";
 import Waves from "../components/Waves";
-import { FirstForm } from "../components/TokenForm";
 import Background from '../img/wave.svg';
 import '../css/pages/TokenRegisterPage.css';
 import '../css/login.css';
@@ -9,6 +8,12 @@ import '../css/login.css';
 export const TokenRegisterPage = () => {
     const [imageSrc, setImageSrc] = useState(null);
     const [abbreviation, setAbbreviation] = useState('');
+    const [tokenName, setTokenName] = useState('');
+    const [companyDescription, setCompanyDescription] = useState('');
+    const [investorBenefits, setInvestorBenefits] = useState('');
+    const [pricePerToken, setPricePerToken] = useState('');
+    const [tokenQuantity, setTokenQuantity] = useState('');
+    const [members, setMembers] = useState(['']);
     const fileInputRef = useRef(null);
 
     const handleDrop = (event) => {
@@ -29,15 +34,7 @@ export const TokenRegisterPage = () => {
         const file = event.target.files[0];
         loadImage(file);
     };
-    const [step, setStep] = useState(0);
 
-    const handleNext = () => {
-        setStep((prevStep) => Math.min(prevStep + 1, 2)); // Incrementa el paso, pero no más de 2
-    };
-
-    const handleBack = () => {
-        setStep((prevStep) => Math.max(prevStep - 1, 0)); // Decrementa el paso, pero no menos de 0
-    };
     const loadImage = (file) => {
         if (file && file.type.startsWith('image/')) {
             const reader = new FileReader();
@@ -48,75 +45,153 @@ export const TokenRegisterPage = () => {
         }
     };
 
-    const handleChange = (event) => {
+    const handleAbbreviationChange = (event) => {
         setAbbreviation(event.target.value.toUpperCase());
     };
 
-    const handleKeyPress = (event) => {
-        const charCode = event.keyCode || event.which;
-        const char = String.fromCharCode(charCode);
+    const handleTokenNameChange = (event) => {
+        setTokenName(event.target.value);
+    };
 
-        if (!/^[a-zA-Z]$/.test(char)) {
-            event.preventDefault();
+    const handleCompanyDescriptionChange = (event) => {
+        setCompanyDescription(event.target.value);
+    };
+
+    const handleInvestorBenefitsChange = (event) => {
+        setInvestorBenefits(event.target.value);
+    };
+
+    const handlePricePerTokenChange = (event) => {
+        setPricePerToken(event.target.value);
+    };
+
+    const handleTokenQuantityChange = (event) => {
+        setTokenQuantity(event.target.value);
+    };
+
+    const handleMemberChange = (index, event) => {
+        const newMembers = [...members];
+        newMembers[index] = event.target.value;
+        setMembers(newMembers);
+    };
+
+    const handleAddMember = () => {
+        if (members.length < 4  ) {
+            setMembers([...members, '']);
         }
+    };
+
+    const [step, setStep] = useState(0);
+
+    const handleNext = () => {
+        setStep((prevStep) => Math.min(prevStep + 1, 2)); // Incrementa el paso, pero no más de 2
+    };
+
+    const handleBack = () => {
+        setStep((prevStep) => Math.max(prevStep - 1, 0)); // Decrementa el paso, pero no menos de 0
     };
 
     return (
         <>
             <Navbar />
             <div className="token-reg">
-                <div className="tokContainer">                    
-                <div className="titu-tok">Registra tu token</div>
-                    {step === 0 ? ( 
-                        <input placeholder="Nombre del Token" className="input2" type="text" />   
-                    ):step === 1 ? (
-                        <input placeholder={`Descripción de la empresa`} className="input3" type="text" />   
-                    ):(<div></div>)}
-                    {step === 0 ? ( 
-                        <input 
-                        placeholder="Abreviación (3 letras)" 
-                        className="input2" 
-                        type="text" 
-                        maxLength="3" 
-                        value={abbreviation} 
-                        onChange={handleChange}
-                        onKeyPress={handleKeyPress}
-                        />                    
-                    ):(<div></div>)}
-                    {step === 0 ? ( 
-                    <div className="caja-tok">
-                        <div className="caja-tok2">
-                            <div className="sub-titu-tok">Logo del token</div>
-                            <div 
-                                className="circ" 
-                                onDrop={handleDrop} 
-                                onDragOver={handleDragOver} 
-                                onClick={handleClick}
-                            >
-                                {imageSrc ? (
-                                    <img src={imageSrc} alt="Token Logo" style={{                 
-                                        width: '100%', 
-                                        height: '100%', 
-                                        objectFit: 'cover' 
-                                }} />
-                                ) : (
-                                    "Inserte aquí"
-                                )}
-                            </div>
-                            <input 
-                                type="file" 
-                                ref={fileInputRef} 
-                                style={{ display: 'none' }} 
-                                onChange={handleFileChange} 
-                                accept="image/*"
+                <div className="tokContainer">
+                    <div className="titu-tok">Registra tu token</div>
+                    {step === 0 ? (
+                        <>
+                            <input
+                                placeholder="Nombre del Token"
+                                className="input2"
+                                type="text"
+                                value={tokenName}
+                                onChange={handleTokenNameChange}
                             />
-                        </div>
-                    </div>
-                    ):step === 1 ?(
-                        <input placeholder="Beneficio para inversores" className="input3" type="text" />   
-                    ):
-                    (<div></div>)}
-                    
+                            <input
+                                placeholder="Abreviación (3 letras)"
+                                className="input2"
+                                type="text"
+                                maxLength="3"
+                                value={abbreviation}
+                                onChange={handleAbbreviationChange}
+                            />
+                            <div className="caja-tok">
+                                <div className="caja-tok2">
+                                    <div className="sub-titu-tok">Logo del token</div>
+                                    <div
+                                        className="circ"
+                                        onDrop={handleDrop}
+                                        onDragOver={handleDragOver}
+                                        onClick={handleClick}
+                                    >
+                                        {imageSrc ? (
+                                            <img src={imageSrc} alt="Token Logo" style={{
+                                                width: '100%',
+                                                height: '100%',
+                                                objectFit: 'cover'
+                                            }} />
+                                        ) : (
+                                            "Inserte aquí"
+                                        )}
+                                    </div>
+                                    <input
+                                        type="file"
+                                        ref={fileInputRef}
+                                        style={{ display: 'none' }}
+                                        onChange={handleFileChange}
+                                        accept="image/*"
+                                    />
+                                </div>
+                            </div>
+                        </>
+                    ) : step === 1 ? (
+                        <>
+                            <textarea
+                                placeholder="Descripción de la empresa"
+                                className="input3"
+                                type="text"
+                                value={companyDescription}
+                                onChange={handleCompanyDescriptionChange}
+                            />
+                            <textarea
+                                placeholder="Beneficio para inversores"
+                                className="input3"
+                                rows="4"
+                                value={investorBenefits}
+                                onChange={handleInvestorBenefitsChange}
+                            />
+                        </>
+                    ) : step === 2 ? (
+                        <>
+                            <div className='sub-miembros'>Miembros de la empresa</div>
+                            {members.map((member, index) => (
+                                <input
+                                    key={index}
+                                    placeholder="Ej: Marcelo Pérez"
+                                    className="input2"
+                                    type="text"
+                                    value={member}
+                                    onChange={(e) => handleMemberChange(index, e)}
+                                />
+                            ))}
+                            {members.length < 4 && (
+                                <button className='but-mas' onClick={handleAddMember}>+</button>
+                            )}                            <div className="caja-tokk">
+                                <input
+                                    className='input4'
+                                    placeholder='Precio por Token (USD)'
+                                    value={pricePerToken}
+                                    onChange={handlePricePerTokenChange}
+                                />
+                                <input
+                                    className='input4'
+                                    placeholder='Cantidad de tokens a generar'
+                                    value={tokenQuantity}
+                                    onChange={handleTokenQuantityChange}
+                                />
+                            </div>
+                        </>
+                    ) : (<div></div>)}
+
                     <div className="caja-buts">
                         <button className="but-volver" onClick={handleBack}>Volver</button>
                         {step === 2 ? (
